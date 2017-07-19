24114ed176f91f4f46c8567fc2e2f76eaf3414dd
markwh <mark.hagemann@gmail.com>
2017-07-19 13:54:12 -0400

    added Po QWBM data and dataset generating script

---
+++ inst/oneoff/datasets.R
@@ -0,0 +1,30 @@
+# Datasets that bamr will store
+
+library(dplyr)
+library(tidyr)
+load("../SWOT/cache/Pepsi.RData")
+
+Po <- Pepsi %>% 
+  filter(name == "Po") %>% 
+  transmute(xs, time, dA, H, S, W, QWBM, Q, A0 = Ao)
+
+Po_w <- Po %>% 
+  select(xs, time, W) %>% 
+  spread(key = xs, value = W, convert = TRUE) %>% 
+  as.matrix()
+
+Po_s <- Po %>% 
+  select(xs, time, S) %>% 
+  spread(key = xs, value = S, convert = TRUE) %>% 
+  as.matrix()
+
+Po_dA <- Po %>% 
+  select(xs, time, dA) %>% 
+  spread(key = xs, value = dA, convert = TRUE) %>% 
+  as.matrix()
+
+Po_QWBM <- Po %>% 
+  select(xs, time, QWBM) %>% 
+  group_by(time) %>% 
+  summarize(QWBM = median(QWBM)) %>% 
+  `[[`("QWBM")
