d4c4774b85f11c2949d053f7e91e6ea9b3675311
markwh <mark.hagemann@gmail.com>
2017-07-24 17:18:56 -0400

    dataset dimension changes as per previous commit

--- inst/oneoff/datasets.R
+++ inst/oneoff/datasets.R
@@ -12,22 +12,26 @@ Po_w <- Po %>%
   select(xs, time, W) %>% 
   spread(key = xs, value = W, convert = TRUE) %>% 
   select(-time) %>% 
-  as.matrix()
+  as.matrix() %>% 
+  t()
 
 Po_s <- Po %>% 
   select(xs, time, S) %>% 
   spread(key = xs, value = S, convert = TRUE) %>% 
   select(-time) %>% 
-  as.matrix()
+  as.matrix() %>% 
+  t()
 
 Po_dA <- Po %>% 
   select(xs, time, dA) %>% 
   spread(key = xs, value = dA, convert = TRUE) %>% 
   select(-time) %>% 
-  as.matrix()
+  as.matrix() %>% 
+  t()
 
 Po_QWBM <- Po %>% 
   select(xs, time, QWBM) %>% 
   group_by(time) %>% 
   summarize(QWBM = median(QWBM)) %>% 
   `[[`("QWBM")
+
