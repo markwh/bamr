53a3c3ee006826b0c62534de353a28f1cf69b3a8
markwh <mark.hagemann@gmail.com>
2017-11-06 09:20:04 -0500

    added tests for meas error

--- tests/testthat/test-estimate.R
+++ tests/testthat/test-estimate.R
@@ -9,8 +9,31 @@ test_that("BAM estimates return expected values", {
   expect_is(bdm <- bam_data(w = Sac_w_sm, s = Sac_s_sm, dA = Sac_dA_sm, Qhat = Sac_QWBM_sm),
             "bamdata")
   
-  expect_is(est1 <- bam_estimate(bda, "amhg"), c("stanfit"))
-  expect_is(est2 <- bam_estimate(bdm, "manning"), c("stanfit"))
+  myseed <- 582
   
+  bp1 <- bam_priors(bda, Werr_sd = 1600, variant = "amhg")
+  expect_is(est1 <- bam_estimate(bda, "amhg", bampriors = bp1, seed = myseed), c("stanfit"))
+  expect_is(est2 <- bam_estimate(bdm, "manning", seed = myseed), c("stanfit"))
+  expect_is(est3 <- bam_estimate(bdm, "manning", seed = myseed), c("stanfit"))
   
+  expect_equivalent(est2, est3)
+  
+  # Now check meas_error functionality
+  expect_is(est4 <- bam_estimate(bda, "amhg", meas_error = FALSE, seed = myseed), c("stanfit"))
+  expect_is(est5 <- bam_estimate(bdm, "manning", meas_error = FALSE, seed = myseed), c("stanfit"))
+  
+  expect_is(qp1 <- bam_qpred(est1), "data.frame")
+  expect_is(qp2 <- bam_qpred(est2), "data.frame")
+  expect_is(qp4 <- bam_qpred(est4), "data.frame")
+  expect_is(qp5 <- bam_qpred(est5), "data.frame")
+  
+  ## (Commented out because AMHG isn't behaving re: meas. error)
+  # expect_true(sum((qp1$conf.high - qp1$conf.low) <= 
+  #                 (qp4$conf.high - qp4$conf.low)) 
+  #             == 0)
+  
+  expect_true(sum((qp2$conf.high - qp2$conf.low) <= 
+                    (qp5$conf.high - qp5$conf.low)) 
+              == 0)
 })
+
